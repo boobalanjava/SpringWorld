2832e93c5e44c8636ada0e591056e32d389ca5ce
boobalansami123 <63717848+boobalansami123@users.noreply.github.com>
2020-06-17 22:13:42 +0530

    New Project

---
+++ SpringCloudClient1/src/main/java/com/spring/client/SpringCloudClient1Application.java
@@ -0,0 +1,13 @@
+package com.spring.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudClient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudClient1Application.class, args);
+	}
+
+}
---
+++ SpringCloudClient1/src/main/java/com/spring/client/controller/ClientController.java
@@ -0,0 +1,25 @@
+package com.spring.client.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientController {
+	
+	@Autowired Environment env;
+	
+	
+	@GetMapping("getConfigValue/{key}")
+	public String getConfigValue(@PathVariable String key) {
+		
+		System.out.println(":::::: GetConfig Value :::::::::"+env.getProperty(key));
+		return key +" : "+env.getProperty(key);  
+		
+	}
+	
+
+}
